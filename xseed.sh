08ce27b04c1c3487dc5ff98118a479abe1591561
bakerboy448 <55419169+bakerboy448@users.noreply.github.com>
2022-10-27 12:57:17 -0500

    Create xseed.sh

---
+++ xseed.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+
+clientname="Qbit"
+xseed_host="localhost"
+xseed_port="2468"
+
+if [ -z "$radarr_eventtype" ]; then
+    app="radarr"
+    clientID=${radarr_download_id}
+    downloadID=${radarr_download_id}
+    eventType=${radarr_eventtype}
+elif [ -z "$sonarr_eventtype" ]; then
+    app="sonarr"
+    clientID=${sonarr_download_client}
+    downloadID=${sonarr_download_id}
+    eventType=${sonarr_eventtype}
+fi
+
+if [ "$eventType" == "Test" ]; then
+    echo "Test passed for $app. DownloadClient: $clientID and DownloadId: $downloadID"
+    exit 0
+fi
+
+if [ -z "$downloadID" ]; then
+    echo "DownloadID is empty from $app. Skipping Xseed. DownloadClient: $clientID and DownloadId: $downloadID"
+    exit 0
+else
+    echo "Client $clientname trigged search for DownloadId $downloadID"
+    curl -s -o /dev/null -w "%{http_code}"-XPOST http://"$xseed_host":"$xseed_port"/api/webhook --data-urlencode infoHash="$downloadID"
+fi
+if [ "$http_code" == "204" ]; then
+    echo "$http_code - Success. Xseed Search triggered by $app for DownloadClient: $clientID and DownloadId: $downloadID"
+    exit 0
+else
+    echo "Xseed webhook failed - HTTP Code $http_code from $app for DownloadClient: $clientID and DownloadId: $downloadID"
+    exit 1
+fi
