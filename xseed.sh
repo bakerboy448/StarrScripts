355bc62566332eee8ff38b387b5be95c9d0f465d
55419169+bakerboy448@users.noreply.github.com <55419169+bakerboy448@users.noreply.github.com>
2022-10-27 15:37:26 -0500

    log for davo

--- xseed.sh
+++ xseed.sh
@@ -11,6 +11,7 @@ if [ -z "$radarr_eventtype" ]; then
     eventType=${radarr_eventtype}
 elif [ -z "$sonarr_eventtype" ]; then
     app="sonarr"
+    echo "sonarr detected with event type $sonarr_eventtype"
     clientID=${sonarr_download_client}
     downloadID=${sonarr_download_id}
     eventType=${sonarr_eventtype}
@@ -19,13 +20,15 @@ else
     exit 1
 fi
 
+echo "$app detected with event type $eventType"
+
 if [ "$eventType" == "Test" ]; then
     echo "Test passed for $app. DownloadClient: $clientID and DownloadId: $downloadID"
     exit 0
 fi
 
 if [ -z "$downloadID" ]; then
-    echo "DownloadID is empty from $app. Skipping Xseed. DownloadClient: $clientID and DownloadId: $downloadID"
+    echo "DownloadID is empty from $app. Skipping cross-seed search. DownloadClient: $clientID and DownloadId: $downloadID"
     exit 0
 fi
 if [ "$clientID" == "$clientname" ]; then
@@ -36,7 +39,7 @@ else
     echo "Client $clientID is not configured $clientname. Skipping..."
     exit 0
 fi
-if [ "$xseed_resp" == "204" ]; then
+if [ "$xseed_resp" == "204" ]; then # 204 = Success per Xseed docs
     echo "Success. Xseed Search triggered by $app for DownloadClient: $clientID and DownloadId: $downloadID"
     exit 0
 else
