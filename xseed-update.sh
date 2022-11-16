10c8c0d001d544fd85beb7b2a7d90f7679ec1340
bakerboy448 <55419169+bakerboy448@users.noreply.github.com>
2022-11-15 21:45:24 -0600

    Create xseed-update.sh

---
+++ xseed-update.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+PKGNAME="cross-seed"
+SERVICENAME="xseed"
+sudo npm uninstall --location=global "$PKGNAME"
+sudo npm install --location=global "$PKGNAME"
+sudo systemctl restart "$SERVICENAME"
