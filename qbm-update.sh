928b98cfc103c7fce182085f37733b0817c20070
bakerboy448 <55419169+bakerboy448@users.noreply.github.com>
2022-11-16 10:06:27 -0600

    [qbm] only install requirements if changed
    - improve logging
    - add more variables
    - don't cd to folder
    - add better logic

--- qbm-update.sh
+++ qbm-update.sh
@@ -1,20 +1,50 @@
 #!/bin/bash
 
+# Get Current User
+uid=$(id -u)
+
+# Set Variables
 qbmPath="/opt/QbitManage"
-qbmVenvPath="$qbmPath"/"qbit-venv/"
+qbmVenvName="qbit-venv"
 qbmServiceName="qbmanage"
-cd "$qbmPath" || exit
-currentVersion=$(cat VERSION)
-branch=$(git rev-parse --abbrev-ref HEAD)
-git fetch
-if [ "$(git rev-parse HEAD)" = "$(git rev-parse @'{u}')" ]; then
+qbmUpstreamGitRemote="origin"
+
+# Create Paths
+qbmVersionFile="$qbmPath/VERSION"
+qbmRequirementsFile="$qbmPath/requirements.txt"
+qbmVenvPath="$qbmPath"/"$qbmVenvName"
+currentVersion=$(cat "$qbmVersionFile")
+currentRequirements=$(sha1sum "$qbmRequirementsFile" | awk '{print $1}')
+
+# Check if qbm is installed & Get Repo Owner
+# Check if user executing script owns qbm Repo
+if [ -d "$qbmPath" ]; then
+    qbmRepoOwner=$(stat -c '%u' "$qbmPath")
+    if [ "$qbmRepoOwner" != "$uid" ]; then
+        echo "QbitManage folder does exist but you [$uid] do not own the repo. Please run this script as the user that owns the repo [$qbmRepoOwner]"
+        exit 1
+    fi
+else
+    echo "QbitManage folder does not exist. Please install QbitManage before running this script."
+    exit 1
+fi
+
+# Get current Branch
+branch=$(git -C "$qbmPath" rev-parse --abbrev-ref HEAD)
+echo "Current Branch: $branch. Checking for updates..."
+git -C "$qbmPath" fetch
+if [ "$(git -C "$qbmPath" rev-parse HEAD)" = "$(git -C "$qbmPath" rev-parse @'{u}')" ]; then
     echo "=== Already up to date $currentVersion on $branch ==="
     exit 0
 fi
-git pull
-newVersion=$(cat VERSION)
-"$qbmVenvPath"/bin/python -m pip install -r requirements.txt
-echo "=== Updated from $currentVersion to $newVersion on $branch ==="
+git -C "$qbmPath" reset --hard "$qbmUpstreamGitRemote"/"$branch"
+newVersion=$(cat "$qbmVersionFile")
+newRequirements=$(sha1sum "$qbmRequirementsFile" | awk '{print $1}')
+if [ "$currentRequirements" != "$newRequirements" ]; then
+    echo "=== Requirements changed, updating venv ==="
+    "$qbmVenvPath"/bin/pip install -r "$qbmRequirementsFile"
+fi
 echo "=== Restarting qbm Service ==="
 sudo systemctl restart "$qbmServiceName"
+echo "=== Updated from $currentVersion to $newVersion on $branch"
 exit 0
