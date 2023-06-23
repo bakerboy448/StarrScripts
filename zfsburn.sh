e1e5af0b377a8d23a4a1817b422ab0148ffe17d7
bakerboy448 <55419169+bakerboy448@users.noreply.github.com>
2023-06-23 12:47:28 -0500

    Update zfsburn.sh -v2

--- zfsburn.sh
+++ zfsburn.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Constants
-VERBOSE=1  # Set this to 1 for trace-level logging, 0 for informational logging
+VERBOSE=1 # Set this to 1 for trace-level logging, 0 for informational logging
 MAX_FREQ=2
 MAX_HOURLY=2
 MAX_DAILY=0
@@ -21,12 +21,10 @@ log() {
 get_snapshot_count() {
     local snapshot_type="$1"
     local dataset="$2"
+    local snapshot_count=0
 
     # Filter snapshots based on the snapshot type and count them
-    snapshot_count=$(sudo zfs list -t snapshot -o name -r "$dataset" | grep -cE "@$snapshot_type-[0-9]{4}-[0-9]{2}-[0-9]{2}-[0-9]{4}$")
-
-    # Verbose logging
-    log 1 "Snapshot Count for type $snapshot_type: $snapshot_count"
+    snapshot_count=$(sudo zfs list -t snapshot -o name -r "$dataset" | grep -cE "$dataset@.*$snapshot_type-[0-9]{4}-[0-9]{2}-[0-9]{2}-[0-9]{4}$")
     # Return the snapshot count as a variable
     echo "$snapshot_count"
 }
@@ -42,38 +40,44 @@ delete_snapshots() {
     readarray -t snapshots < <(sudo zfs list -t snapshot -H -o name -r "$dataset")
 
     # Info log prior to filtering
-    log 0 "Total snapshots before filtering: ${#snapshots[@]}"
+    log 0 "Total snapshots before filtering: [${#snapshots[@]}]"
 
     # Loop through snapshots and delete based on frequency limits
     for snapshot in "${snapshots[@]}"; do
-        log 1 "Filtering snapshot: $snapshot"
+        log 1 "Filtering snapshot: [$snapshot]"
 
         local snapshot_name=${snapshot##*/}
         local snapshot_type=${snapshot_name#*_}
         snapshot_type=${snapshot_type%%-*}
 
         if [[ "$snapshot_type" == "frequent" || "$snapshot_type" == "hourly" || "$snapshot_type" == "daily" || "$snapshot_type" == "weekly" || "$snapshot_type" == "monthly" ]]; then
-            log 0 "Processing snapshot: $snapshot"
-            
+            log 0 "Processing snapshot: [$snapshot]"
+
             local max_count=0
+            local current_count=0
 
             if [[ "$snapshot_type" == "frequent" ]]; then
                 max_count=$MAX_FREQ
+                current_count=$frequent_count
             elif [[ "$snapshot_type" == "hourly" ]]; then
                 max_count=$MAX_HOURLY
+                current_count=$hourly_count
             elif [[ "$snapshot_type" == "daily" ]]; then
                 max_count=$MAX_DAILY
+                current_count=$daily_count
             elif [[ "$snapshot_type" == "weekly" ]]; then
                 max_count=$MAX_WEEKLY
+                current_count=$weekly_count
             elif [[ "$snapshot_type" == "monthly" ]]; then
                 max_count=$MAX_MONTHLY
+                current_count=$monthly_count
             fi
 
-            log 1 "Current snapshot count: $snapshot_count"
-            log 1 "Maximum allowed: $max_count"
+            log 1 "Current snapshot count: [$current_count]"
+            log 1 "Maximum allowed: [$max_count]"
 
-            if ((snapshot_count > max_count || max_count == 0)); then
-                log 0 "Deleting snapshot: $snapshot"
+            if ((current_count > max_count || max_count == 0)); then
+                log 0 "Deleting snapshot: [$snapshot]"
 
                 local snapshot_space
                 snapshot_space=$(sudo zfs list -o used -H -p "$snapshot" | awk '{print $1}')
@@ -81,17 +85,17 @@ delete_snapshots() {
                 if sudo zfs destroy "$snapshot"; then
                     ((deleted++))
                     ((space_gained += snapshot_space))
-                    log 0 "Space gained: $(printf "%.2f" "$(bc -l <<< "scale=2; $snapshot_space / 1024")") KB"
+                    log 0 "Space gained: $(printf "%.2f" "$(bc -l <<<"scale=2; $snapshot_space / 1024")") KB"
                 else
-                    log 0 "Error deleting snapshot: $snapshot"
+                    log 0 "Error deleting snapshot: [$snapshot]"
                 fi
             fi
         else
-            log 1 "Skipped processing snapshot: $snapshot - no match to type: $snapshot_type"
+            log 1 "Skipped processing snapshot: [$snapshot] - no match to type: [$snapshot_type]"
         fi
     done
 
-    log 0 "Deleted $deleted snapshots for dataset: $dataset. Total space gained: $(printf "%.2f" "$(bc -l <<< "scale=2; $space_gained / 1024")") KB"
+    log 0 "Deleted $deleted snapshots for dataset: [$dataset]. Total space gained: $(printf "%.2f" "$(bc -l <<<"scale=2; $space_gained / 1024")") KB"
 }
 
 # Usage: ./zfsburn.sh <dataset>
@@ -110,10 +114,10 @@ weekly_count=$(get_snapshot_count "weekly" "${datasets[@]}")
 monthly_count=$(get_snapshot_count "monthly" "${datasets[@]}")
 
 # Use the snapshot counts as needed in the rest of the script
-log 0 "Frequent Snapshot Count: $frequent_count"
-log 0 "Hourly Snapshot Count: $hourly_count"
-log 0 "Daily Snapshot Count: $daily_count"
-log 0 "Weekly Snapshot Count: $weekly_count"
-log 0 "Monthly Snapshot Count: $monthly_count"
+log 0 "Frequent Snapshot Count: [$frequent_count]"
+log 0 "Hourly Snapshot Count: [$hourly_count]"
+log 0 "Daily Snapshot Count: [$daily_count]"
+log 0 "Weekly Snapshot Count: [$weekly_count]"
+log 0 "Monthly Snapshot Count: [$monthly_count]"
 
 delete_snapshots "${datasets[@]}"
