dfd69147ae03a8378c32694061d88b4084128067
bakerboy448 <55419169+bakerboy448@users.noreply.github.com>
2023-08-20 08:44:43 -0500

    Update qbm-qbit.sh

--- qbm-qbit.sh
+++ qbm-qbit.sh
@@ -10,11 +10,12 @@ another_instance() {
 }
 lockfile -r 0 -l 3600 "$LOCK" || another_instance
 trap remove_lock EXIT
-sleep 60
+sleep 600
 # -cs = cross-seed
 # -re = recheck
 # -cu = cat-update
 # -tu = tag-update
 # -ru = remove unregistered
 # Do not remove orphaned torrents as imports may be in-progress
-"$PATH_QBM"/qbit-venv/bin/python "$PATH_QBM"/qbit_manage.py -cs -re -cu -tu -ru -r --config-file /data/media/.config/QbitMngr/config.yml
+# -sl = share limits
+/opt/.venv/qbm-venv/bin/python "$PATH_QBM"/qbit_manage.py -cs -re -cu -tu -ru -sl  -r --config-file /data/media/.config/QbitMngr/config.yml
