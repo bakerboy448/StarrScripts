ab3a1b7c7677765ca649a6f09d9a07fc929e4be1
bakerboy448 <55419169+bakerboy448@users.noreply.github.com>
2023-08-20 08:26:51 -0500

    Update notifiarr-branch-builder.sh

--- notifiarr-branch-builder.sh
+++ notifiarr-branch-builder.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 # Define variables
+repo_url="https://github.com/Notifiarr/notifiarr.git"
 repo_dir="/home/bakerboy448/notifiarr"
 source_path="$repo_dir/notifiarr"
 bin_path="/usr/bin/notifiarr"
@@ -12,6 +13,21 @@ handle_error() {
     exit 1
 }
 
+# Check if user wants to reinstall using apt
+read -p "Do you want to reinstall Notifiarr using apt? [Y/n] " apt_choice
+
+if [[ "$apt_choice" == [Yy]* ]]; then
+    sudo apt update && sudo apt install --reinstall notifiarr || handle_error "Failed to reinstall Notifiarr using apt."
+    exit 0
+fi
+
+# Clone the repo if it doesn't exist, else fetch the latest
+if [[ ! -d "$repo_dir" ]]; then
+    git clone "$repo_url" "$repo_dir" || handle_error "Failed to clone repository."
+else
+    git -C "$repo_dir" fetch --all || handle_error "Failed to fetch updates from remote."
+fi
+
 # Get the current branch
 current_branch=$(git -C "$repo_dir" rev-parse --abbrev-ref HEAD)
 echo "Current branch is: $current_branch"
@@ -19,10 +35,10 @@ read -p "Do you want to use the current branch? [Y/n] " choice
 
 if [[ "$choice" != [Yy]* ]]; then
     # List all available branches
-    branches=$(git -C "$repo_dir" branch | sed 's/* //')
+    branches=$(git -C "$repo_dir" branch -r | sed 's/origin\///' | sed 's/* //')
     echo "Available branches:"
     echo "$branches"
-    
+
     while true; do
         read -p "Enter the branch name you want to use: " branch
         if [[ $branches =~ $branch ]]; then
@@ -41,7 +57,7 @@ fi
 # Pull latest changes from the selected branch
 git -C "$repo_dir" pull || handle_error "Failed to pull latest changes."
 
-# Compile the code
+# Compile the code (assuming the repository requires a 'make' step)
 make --directory="$repo_dir" || handle_error "Failed to compile."
 
 # Change owner of the compiled binary
