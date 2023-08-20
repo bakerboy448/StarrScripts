3460a0755a9cf2d84619ee42a905532cf1b039dc
bakerboy448 <55419169+bakerboy448@users.noreply.github.com>
2023-08-20 08:46:47 -0500

    Update omegabrr_upgrade.sh

--- omegabrr_upgrade.sh
+++ omegabrr_upgrade.sh
@@ -1,13 +1,29 @@
 #!/bin/bash
-old_version="$(omegabrr version)"
-dlurl="$(curl -s https://api.github.com/repos/autobrr/omegabrr/releases/latest | grep download | grep linux_x86_64 | cut -d\" -f4)"
-wget "$dlurl"
-sudo tar -xzf omegabrr*.tar.gz
-sudo mv omegabrr /usr/bin/omegabrr
-rm omegabrr*.tar.gz
-echo "Omegabrr Updated"
-echo "Old Version"
-echo "$old_version"
-echo "New Version"
-omegabrr version
+
+# Get the old version of omegabrr
+old_version=$(omegabrr version)
+
+# Fetch the URL of the latest release for linux_x86_64
+dlurl=$(curl -s https://api.github.com/repos/autobrr/omegabrr/releases/latest | grep -E 'browser_download_url.*linux_x86_64' | cut -d\" -f4)
+
+# Download the latest release
+if [ -n "$dlurl" ]; then
+    wget "$dlurl"
+    # Extract the downloaded archive
+    sudo tar -xzf omegabrr*.tar.gz
+    # Move omegabrr to /usr/bin
+    sudo mv omegabrr /usr/bin/omegabrr
+    # Clean up downloaded files
+    rm omegabrr*.tar.gz
+    echo "Omegabrr Updated"
+else
+    echo "Failed to fetch download URL. Exiting..."
+    exit 1
+fi
+
+# Display old and new versions
+echo "Old Version: $old_version"
+echo "New Version: $(omegabrr version)"
+
+# Restart the omegabrr service (assuming sysrestart command exists)
 sysrestart omegabrr@bakerboy448
