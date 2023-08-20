337d40b85cd6997cacef87e4d46fe064328d5c0f
bakerboy448 <55419169+bakerboy448@users.noreply.github.com>
2023-08-20 09:53:58 -0500

    Update pic-update.sh

--- pic-update.sh
+++ pic-update.sh
@@ -23,4 +23,31 @@ check_pic_installation() {
         fi
     else
         echo "Plex-Image-Cleanup folder does not exist. Please install Plex-Image-Cleanup before running this script."
-        exit
+        exit 1
+    fi
+}
+
+# Update the Plex-Image-Cleanup repository
+update_pic_repository() {
+    echo "Updating Plex-Image-Cleanup repository..."
+    cd "$PIC_PATH"
+    git pull "$PIC_UPSTREAM_GIT_REMOTE" master
+}
+
+# Update the Python virtual environment
+update_python_venv() {
+    echo "Updating Python virtual environment..."
+    source "$PIC_VENV_PATH/bin/activate"
+    pip install -r "$PIC_REQUIREMENTS_FILE"
+}
+
+# Main function
+main() {
+    check_pic_installation
+    update_pic_repository
+    update_python_venv
+
+    echo "Plex-Image-Cleanup has been updated successfully."
+}
+
+main
