a21d2fdeda65e8a24b20d8db0459681c2cc793a5
bakerboy448 <55419169+bakerboy448@users.noreply.github.com>
2023-08-20 09:58:32 -0500

    Update pic-update.sh

--- pic-update.sh
+++ pic-update.sh
@@ -13,7 +13,7 @@ PIC_VERSION_FILE="$PIC_PATH/VERSION"
 PIC_REQUIREMENTS_FILE="$PIC_PATH/requirements.txt"
 CURRENT_UID=$(id -u)
 
-# Check if PIC is installed and if the current user owns it
+# Check if Plex-Image-Cleanup is installed and the current user owns it
 check_pic_installation() {
     if [ -d "$PIC_PATH" ]; then
         local pic_repo_owner=$(stat -c '%u' "$PIC_PATH")
@@ -27,27 +27,39 @@ check_pic_installation() {
     fi
 }
 
-# Update the Plex-Image-Cleanup repository
-update_pic_repository() {
-    echo "Updating Plex-Image-Cleanup repository..."
-    cd "$PIC_PATH"
-    git pull "$PIC_UPSTREAM_GIT_REMOTE" master
+# Update Plex-Image-Cleanup if necessary
+update_pic() {
+    local current_branch=$(git -C "$PIC_PATH" rev-parse --abbrev-ref HEAD)
+    echo "Current Branch: $current_branch. Checking for updates..."
+    git -C "$PIC_PATH" fetch
+    if [ "$(git -C "$PIC_PATH" rev-parse HEAD)" = "$(git -C "$PIC_PATH" rev-parse @'{u}')" ] && [ "$force_update" != true ]; then
+        local current_version=$(cat "$PIC_VERSION_FILE")
+        echo "=== Already up to date $current_version on $current_branch ==="
+        exit 0
+    fi
+    git -C "$PIC_PATH" reset --hard "$PIC_UPSTREAM_GIT_REMOTE/$current_branch"
 }
 
-# Update the Python virtual environment
-update_python_venv() {
-    echo "Updating Python virtual environment..."
-    source "$PIC_VENV_PATH/bin/activate"
-    pip install -r "$PIC_REQUIREMENTS_FILE"
+# Update venv if necessary
+update_venv() {
+    local current_requirements=$(sha1sum "$PIC_REQUIREMENTS_FILE" | awk '{print $1}')
+    local new_requirements=$(sha1sum "$PIC_REQUIREMENTS_FILE" | awk '{print $1}')
+    if [ "$current_requirements" != "$new_requirements" ] || [ "$force_update" = true ]; then
+        echo "=== Requirements changed, updating venv ==="
+        "$PIC_VENV_PATH/bin/python3" "$PIC_VENV_PATH/bin/pip" install -r "$PIC_REQUIREMENTS_FILE"
+    fi
 }
 
-# Main function
-main() {
-    check_pic_installation
-    update_pic_repository
-    update_python_venv
-
-    echo "Plex-Image-Cleanup has been updated successfully."
+# Restart the Plex-Image-Cleanup service
+restart_service() {
+    echo "=== Restarting Plex-Image-Cleanup Service ==="
+    sudo systemctl restart "$PIC_SERVICE_NAME"
+    local new_version=$(cat "$PIC_VERSION_FILE")
+    echo "=== Updated to $new_version on $current_branch ==="
 }
 
-main
+# Main script execution
+check_pic_installation
+update_pic
+update_venv
+restart_service
