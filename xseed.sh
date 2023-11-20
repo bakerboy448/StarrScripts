c704103a1dd83948cdc654ac474f27c36b801c23
Sebastian <69204495+sbstnhp@users.noreply.github.com>
2023-11-20 16:03:57 +0100

    feat(add): cross-seed apikey support, feat(change): only send single file queries to cross-seed

--- xseed.sh
+++ xseed.sh
@@ -4,6 +4,7 @@ torrentclientname="Qbit"
 usenetclientname="SABnzbd"
 xseed_host="127.0.0.1"
 xseed_port="2468"
+xseed_apikey=""
 log_file="/data/media/.config/xseed_db.log"
 
 # Determine app and set variables
@@ -17,7 +18,7 @@ elif [ -n "$sonarr_eventtype" ]; then
     app="sonarr"
     clientID="$sonarr_download_client"
     downloadID="$sonarr_download_id"
-    filePath="$sonarr_series_path"
+    filePath="$sonarr_episodefile_path"
     folderPath="$sonarr_episodefile_sourcefolder"
     eventType="$sonarr_eventtype"
 elif [ -n "$lidarr_eventtype" ]; then
@@ -42,7 +43,11 @@ echo "$app detected with event type $eventType"
 cross_seed_request() {
     local endpoint="$1"
     local data="$2"
-    curl --silent --output /dev/null --write-out "%{http_code}" -X POST "http://$xseed_host:$xseed_port/api/$endpoint" --data-urlencode "$data"
+    if [  -n "$xseed_apikey" ]; then
+        curl --silent --output /dev/null --write-out "%{http_code}" -X POST "http://$xseed_host:$xseed_port/api/$endpoint" -H "X-Api-Key: $xseed_apikey" --data-urlencode "$data"
+    else
+        curl --silent --output /dev/null --write-out "%{http_code}" -X POST "http://$xseed_host:$xseed_port/api/$endpoint" --data-urlencode "$data"
+    fi
 }
 
 # Create the log file if it doesn't exist
@@ -69,12 +74,17 @@ case "$clientID" in
         xseed_resp=$(cross_seed_request "webhook" "infoHash=$downloadID")
         ;;
     "$usenetclientname")
-        if [[ "$folderPath" =~ S[0-9]{1,2}(?!\.E[0-9]{1,2}) ]]; then
-            echo "Client $usenetclientname skipped search for FolderPath $folderPath due to being a SeasonPack for Usenet"
-            exit 0
+        if [ "$app" == "radarr" ] || [ "$app" == "sonarr" ]; then
+            if [[ "$folderPath" =~ S[0-9]{1,2}(?!\.E[0-9]{1,2}) ]]; then
+                echo "Client $usenetclientname skipped search for FolderPath $folderPath due to being a SeasonPack for Usenet"
+                exit 0
+            else
+                echo "Client $usenetclientname triggered data search for DownloadId $downloadID using FilePath $filePath with FolderPath $folderPath"
+                xseed_resp=$(cross_seed_request "webhook" "path=$filePath")
+            fi
         else
-            echo "Client $usenetclientname triggered data search for DownloadId $downloadID using FilePath $filePath with FolderPath $folderPath"
-            xseed_resp=$(cross_seed_request "webhook" "path=$filePath")
+            echo "Client $usenetclientname skipped search for FilePath $filePath due to being not a single file query from $app"
+            exist 0
         fi
         ;;
     *)
