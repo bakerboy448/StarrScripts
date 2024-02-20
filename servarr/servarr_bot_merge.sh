88c0ca4c6a0b6e05e3c3c889dad20f6042a18dad
bakerboy448 <55419169+bakerboy448@users.noreply.github.com>
2024-02-19 19:11:32 -0600

    Update servarr_bot_merge.sh

--- servarr/servarr_bot_merge.sh
+++ servarr/servarr_bot_merge.sh
@@ -5,25 +5,30 @@ REPO_URL="https://github.com/Servarr/Wiki.git"
 TARGET_BRANCH="master"
 COMMIT_BRANCH="update-wiki-supported-indexers"
 
-# Clone the repository
-
-#git clone "$REPO_URL"
-#cd Wiki
+# Navigate to the repository's directory
 cd ~/_development/servarr.wiki
-# Fetch latest updates
+
+# Fetch the latest updates from the origin
 git fetch origin
+
+# Ensure the TARGET_BRANCH is up to date with origin
+git checkout $TARGET_BRANCH
+git pull origin $TARGET_BRANCH
 
 # Checkout to the branch containing the commit you want to rebase
 git checkout "$COMMIT_BRANCH"
 
 # Rebase the commit onto the target branch
-git rebase "$TARGET_BRANCH"
+git rebase origin/"$TARGET_BRANCH"
 
 # Check if the rebase was successful
 if [ $? -eq 0 ]; then
-    # Push the changes back to the repository
-    git push origin "$COMMIT_BRANCH"
-    echo "Rebase and push completed."
+    # Attempt to push the changes back to the repository
+    if git push origin "$COMMIT_BRANCH"; then
+        echo "Rebase and push completed successfully."
+    else
+        echo "Push failed. Please check the remote branch status and resolve any issues."
+    fi
 else
     echo "Rebase encountered conflicts. Resolve them manually and then continue the rebase process."
 fi
