0df006fda5d50e10b1b0eb83bad63709e9fcfd29
bakerboy448 <55419169+bakerboy448@users.noreply.github.com>
2024-04-22 13:20:04 -0500

    Update dupe.sh - paths

--- dupe.sh
+++ dupe.sh
@@ -3,12 +3,12 @@
 jdupes_command="/usr/bin/jdupes"
 exclude_dirs="-X nostr:.RecycleBin -X nostr:.trash"
 include_ext="-X onlyext:mp4,mkv,avi"
-output_log="/home/bakerboy448/jdupes.log"
-source_dir="/data/media/media/"
-destination_dir="/data/media/torrents/"
-hash_db="/home/bakerboy448/jdupes_hashdb"
+output_log="/.config/jdupes.log"
+source_dir="/mnt/data/media/"
+destination_dir="/mnt/data/torrents/"
+hash_db="/.config/jdupes_hashdb"
 media2_dir="/mnt/media2"
-media2_hash_db="/home/bakerboy448/jdupes_hashdb_media2"
+media2_hash_db="/.config/jdupes_hashdb_media2"
 
 timestamp=$(date +"%Y-%m-%d %H:%M:%S")
 echo "[$timestamp] Duplicate search started for $source_dir and $destination_dir." >> "$output_log"
