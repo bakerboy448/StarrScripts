a753cbdd31548443f01a5a6a02a898519304e52c
bakerboy448 <55419169+bakerboy448@users.noreply.github.com>
2024-04-25 00:35:21 -0500

    Update dupe.sh v2

--- dupe.sh
+++ dupe.sh
@@ -1,15 +1,22 @@
 #!/bin/bash
 
+# Load environment variables from .env file
+set -a  # automatically export all variables
+source .env
+set +a
+
+# Command and options
 jdupes_command="/usr/bin/jdupes"
 exclude_dirs="-X nostr:.RecycleBin -X nostr:.trash"
 include_ext="-X onlyext:mp4,mkv,avi"
-output_log="/.config/jdupes.log"
-source_dir="/mnt/data/media/"
-destination_dir="/mnt/data/torrents/"
-hash_db="/.config/jdupes_hashdb"
 
+# Logging the start of the operation
 timestamp=$(date +"%Y-%m-%d %H:%M:%S")
-echo "[$timestamp] Duplicate search started for $source_dir and $destination_dir." >> "$output_log"
-$jdupes_command $exclude_dirs $include_ext -L -r -Z -y "$hash_db" "$source_dir" "$destination_dir" >> "$output_log"
+echo "[$timestamp] Duplicate search started for $JDUPES_SOURCE_DIR and $JDUPES_DESTINATION_DIR." >> "$JDUPES_OUTPUT_LOG"
+
+# Running jdupes with the loaded environment variables
+$jdupes_command $exclude_dirs $include_ext -L -r -Z -y "$JDUPES_HASH_DB" "$JDUPES_SOURCE_DIR" "$JDUPES_DESTINATION_DIR" >> "$JDUPES_OUTPUT_LOG"
+
+# Logging the completion of the operation
 timestamp=$(date +"%Y-%m-%d %H:%M:%S")
-echo "[$timestamp] Duplicate search completed for $source_dir and $destination_dir." >> "$output_log"
+echo "[$timestamp] Duplicate search completed for $JDUPES_SOURCE_DIR and $JDUPES_DESTINATION_DIR." >> "$JDUPES_OUTPUT_LOG"
