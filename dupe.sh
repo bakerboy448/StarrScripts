d67122bf65c20d477b68559fde2a93698f668cd9
bakerboy448 <55419169+bakerboy448@users.noreply.github.com>
2024-04-26 14:38:27 -0500

    fix dupe.sh

--- dupe.sh
+++ dupe.sh
@@ -21,8 +21,9 @@ INCLUDE_EXT=${INCLUDE_EXT:-"-X onlyext:mp4,mkv,avi"}
 # Logging the start of the operation
 timestamp=$(date +"%Y-%m-%d %H:%M:%S")
 echo "[$timestamp] Duplicate search started for $JDUPES_SOURCE_DIR and $JDUPES_DESTINATION_DIR." >>"$JDUPES_OUTPUT_LOG"
-
+echo "command is"
 # Running jdupes with the loaded environment variables
+echo $JDUPES_COMMAND "$EXCLUDE_DIRS" "$INCLUDE_EXT" -L -r -Z -y "$JDUPES_HASH_DB" "$JDUPES_SOURCE_DIR" "$JDUPES_DESTINATION_DIR"
 $JDUPES_COMMAND "$EXCLUDE_DIRS" "$INCLUDE_EXT" -L -r -Z -y "$JDUPES_HASH_DB" "$JDUPES_SOURCE_DIR" "$JDUPES_DESTINATION_DIR" >>"$JDUPES_OUTPUT_LOG"
 
 # Logging the completion of the operation
