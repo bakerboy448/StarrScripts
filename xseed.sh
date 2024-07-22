4860dbbffcdd70c223fb00965de5b86dd43ba52e
bakerboy448 <55419169+bakerboy448@users.noreply.github.com>
2024-07-21 19:22:32 -0500

    update(logging): cleanup and logging improvements

--- xseed.sh
+++ xseed.sh
@@ -9,7 +9,16 @@
 # Load environment variables from .env file if it exists
 if [ -f ".env" ]; then
     # shellcheck source=.env
+    echo "Loading environment variables from .env file"
     source ".env"
+    if [ $? -eq 0 ]; then
+        echo "Environment variables loaded successfully"
+    else
+        echo "Error loading environment variables" >&2
+        exit 1
+    fi
+else
+    echo ".env file not found"
 fi
 
 # Use environment variables with descriptive default values
@@ -19,6 +28,21 @@ XSEED_HOST=${XSEED_HOST:-crossseed}
 XSEED_PORT=${XSEED_PORT:-8080}
 LOG_FILE=${LOG_FILE:-/config/xseed.log}
 XSEED_APIKEY=${XSEED_APIKEY}
+
+# Function to log messages
+log_message() {
+    local log_type="$1"
+    local message="$2"
+    echo "$(date '+%Y-%m-%d %H:%M:%S') [$log_type] $message" | tee -a "$LOG_FILE"
+}
+
+log_message "INFO" "Script started"
+log_message "INFO" "Using environment variables:"
+log_message "INFO" "TORRENT_CLIENT_NAME=$TORRENT_CLIENT_NAME"
+log_message "INFO" "USENET_CLIENT_NAME=$USENET_CLIENT_NAME"
+log_message "INFO" "XSEED_HOST=$XSEED_HOST"
+log_message "INFO" "XSEED_PORT=$XSEED_PORT"
+log_message "INFO" "LOG_FILE=$LOG_FILE"
 
 # Function to send a request to Cross Seed API
 cross_seed_request() {
@@ -67,10 +91,11 @@ detect_application() {
         # shellcheck disable=SC2154 # These are set by Starr on call
         eventType="$sonarr_eventtype"
     fi
-    [ "$app" == "unknown" ] && {
-        echo "Unknown application type detected. Exiting."
+    if [ "$app" == "unknown" ]; then
+        log_message "ERROR" "Unknown application type detected. Exiting."
         exit 1
-    }
+    fi
+    log_message "INFO" "Detected application: $app"
 }
 
 # Validate the process
@@ -78,33 +103,41 @@ validate_process() {
     [ ! -f "$LOG_FILE" ] && touch "$LOG_FILE"
     unique_id="${downloadID}-${clientID}"
 
-    [ -z "$unique_id" ] && return
-    grep -qF "$unique_id" "$LOG_FILE" && {
-        echo "Download ID $unique_id already processed. Exiting."
-        exit 0
-    }
+    if [ -z "$unique_id" ]; then
+        log_message "ERROR" "Unique ID is missing. Exiting."
+        exit 1
+    fi
 
-    [ -z "$eventType" ] && {
-        echo "No event type specified. Exiting."
-        exit 1
-    }
-    [ "$eventType" == "Test" ] && {
-        echo "Test event detected. Exiting."
+    if grep -qF "$unique_id" "$LOG_FILE"; then
+        log_message "INFO" "Download ID $unique_id already processed. Exiting."
         exit 0
-    }
-    [ -z "$filePath"  ] && [ -z "$folderPath" ] && [ -z "$downloadID" ] && {
-        echo "Essential parameters missing. Exiting."
+    fi
+
+    if [ -z "$eventType" ]; then
+        log_message "ERROR" "No event type specified. Exiting."
         exit 1
-    }
+    fi
+
+    if [ "$eventType" == "Test" ]; then
+        log_message "INFO" "Test event detected. Exiting."
+        exit 0
+    fi
+
+    if [ -z "$filePath" ] && [ -z "$folderPath" ] && [ -z "$downloadID" ]; then
+        log_message "ERROR" "Essential parameters missing. Exiting."
+        exit 1
+    fi
 
     if [ -z "$downloadID" ] && [[ -z "$filePath" || -z "$folderPath" ]]; then
-        echo "Download ID is missing. Checking if file path works for data/path based cross-seeding."
+        log_message "ERROR" "Download ID is missing. Checking if file path works for data/path based cross-seeding."
         if [[ -z "$filePath" && -z "$folderPath" ]]; then
-            echo "File and Folder paths are missing. Exiting."
+            log_message "ERROR" "File and Folder paths are missing. Exiting."
             exit 1
         fi
     fi
 }
+
+# Function to send data for search
 send_data_search() {
     if [ -n "$sonarrReleaseType" ] && [ "$sonarrReleaseType" == "SeasonPack" ]; then
         xseed_resp=$(cross_seed_request "webhook" "path=$folderPath")
@@ -112,39 +145,42 @@ send_data_search() {
         xseed_resp=$(cross_seed_request "webhook" "path=$filePath")
     fi
 }
+
 # Main logic for handling operations
 handle_operations() {
     detect_application
     validate_process
     case "$clientID" in
     "$TORRENT_CLIENT_NAME")
-        echo "Processing torrent client operations..."
-        [ -n "$downloadID" ] && { xseed_resp=$(cross_seed_request "webhook" "infoHash=$downloadID"); }
+        log_message "INFO" "Processing torrent client operations..."
+        if [ -n "$downloadID" ]; then
+            xseed_resp=$(cross_seed_request "webhook" "infoHash=$downloadID")
+        fi
         if [ "$xseed_resp" != "204" ]; then
             sleep 15
             send_data_search
         fi
         ;;
     "$USENET_CLIENT_NAME")
-        if [ -z "$sonarrReleaseType" ] && [[ "$folderPath" =~ S[0-9]{1,2}(?!\.E[0-9]{1,2}) ]]; then {
-            echo "Skipping season pack search. Please switch to On Import Complete for Usenet Season Pack Support!"
+        if [ -z "$sonarrReleaseType" ] && [[ "$folderPath" =~ S[0-9]{1,2}(?!\.E[0-9]{1,2}) ]]; then
+            log_message "WARN" "Depreciated Action. Skipping season pack search. Please switch to On Import Complete for Usenet Season Pack Support!"
             exit 0
-        }
         fi
-        echo "Processing Usenet client operations..."
+        log_message "INFO" "Processing Usenet client operations..."
         send_data_search
         ;;
     *)
-        echo "Unrecognized client $clientID. Exiting."
+        log_message "ERROR" "Unrecognized client $clientID. Exiting."
         exit 1
         ;;
     esac
-    echo "Cross-seed API response: $xseed_resp"
+
+    log_message "INFO" "Cross-seed API response: $xseed_resp"
     if [ "$xseed_resp" == "204" ]; then
         echo "$unique_id" >>"$LOG_FILE"
-        echo "Process completed successfully."
+        log_message "INFO" "Process completed successfully."
     else
-        echo "Process failed with API response: $xseed_resp"
+        log_message "ERROR" "Process failed with API response: $xseed_resp"
         exit 1
     fi
 }
