dad235e0048b9e0420b7ade0a322cdefc0fa8c67
bakerboy448 <55419169+bakerboy448@users.noreply.github.com>
2025-01-23 21:04:45 -0600

    Delete xseed-update.sh

--- xseed-update.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env bash
-PKGNAME="cross-seed"
-SERVICENAME="xseed"
-sudo npm uninstall --location=global "$PKGNAME"
-sudo npm install --location=global "$PKGNAME"
-sudo systemctl restart "$SERVICENAME"
