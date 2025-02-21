7e0406f1538a2c0c46f1955f7b663cc1433a5357
bakerboy448 <55419169+bakerboy448@users.noreply.github.com>
2025-02-21 12:37:14 -0600

    fixup!

--- sonarr_dupefinder.sh
+++ sonarr_dupefinder.sh
@@ -1,4 +1,3 @@
-file_count=$(find "$dir" -maxdepth 1 -type f \( -iname "*.mp4" -o -iname "*.mkv" -o -iname "*.avi" -o -iname "*.mov" -o -iname "*.wmv" -o -iname "*.flv" -o -iname "*.webm" -o -iname "*.mpg" -o -iname "*.mpeg" \) | wc -l)
 #!/bin/bash
 
 directory=${1:-.}  # Use provided directory or default to current directory
